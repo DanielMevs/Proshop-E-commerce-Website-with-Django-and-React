7036737f9cd48347e35d95500417de97e18b4d68
DanielMevs <dannymevs@hotmail.com>
2024-01-15 19:49:30 -0500

    set up static files

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 # Create your views here.
 
 @api_view(['GET'])
@@ -26,16 +29,15 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for item in products:
-        if item['_id'] == pk:
-            product = item
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
 
 
